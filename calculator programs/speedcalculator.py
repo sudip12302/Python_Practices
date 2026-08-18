f7edb8d388526df6cacac216378ded1742b15eda
Sudip Bastola <sudipbastola65@gmail.com>
2026-08-18 20:31:15 +0545

    Speed Calculator

---
+++ calculator programs/speedcalculator.py
@@ -0,0 +1,5 @@
+''' taking input from user  calculate speed '''
+distance = float(input("Enter distance in km: "))
+time = float(input("Enter time in hours: "))
+speed = distance / time
+print("Speed is:", speed, "km/h")
