4dac4a10a0e402716ca005b9771912ecac20ed08
Sudip Bastola <sudipbastola65@gmail.com>
2026-09-15 20:39:51 +0545

    Program to check weekday or weekends

--- Week3/weekday_weekend.py
+++ Week3/weekday_weekend.py
@@ -2,6 +2,7 @@
 it’s a weekday the program should print out “Happy weekday! Work
 hard!” else the output should be “Enjoy your weekend! '''
 
+
 str = input("Enter the current day of the week: ")  # Ask the user to enter the current day of the week
 if str in ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:  # Check if the entered day is a weekday
     print("Happy weekday! Work hard!")  # If it is a weekday, print the message for weekdays
