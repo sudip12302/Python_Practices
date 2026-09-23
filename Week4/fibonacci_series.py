27edda8ad7d5bd62a86d82f8e9cfc53b2de0f96e
Sudip Bastola <sudipbastola65@gmail.com>
2026-09-23 20:44:24 +0545

    Progarm to calculate the fibonacci series

--- Week4/fibonacci_series.py
+++ Week4/fibonacci_series.py
@@ -4,4 +4,5 @@ a= [1,1]  # Initialize a list with the first two numbers of the Fibonacci series
 for i in range(2,n):  # Loop from 2 to n (exclusive) to generate the Fibonacci series
     next = a[i-1] + a[i-2]  # Calculate the next number in the series by adding the last two numbers
     a.append(next)  # Add the next number to the list
+    
 print("The Fibonacci series up to", n, "is:", a)  # Print the Fibonacci series
